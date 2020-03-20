3dccdb6564b85566d6c0e1dece392e2a6b6df327
Rickson <rickson.simoes@hotmail.com>
2020-03-20 17:55:14 -0300

    :hammer: :wrench: :package: Adicionado alguns erros, paginas, pacotes e configuraçoes

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,43 @@
-export default function About() {
-  return (
-    <div>
-      <p>This is the about page</p>
-    </div>
-  );
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+
+class Page extends Component {
+
+  state = {
+    valor : ''
+  }
+
+  digitarTexto = e  => {
+    this.setState({valor: e.target.value });
+  }
+
+  enviarErro = e => {
+    e.preventDefault();
+    const { valor } = this.state;
+
+    if (valor) {
+      throw new Error(`Mais um erro: ${valor}`);
+    }
+  }
+
+  render () {
+    return (
+      <>   
+        <h3>Nome do erro: <small><i>{this.state.valor}</i></small></h3>
+
+        <form onSubmit={this.enviarErro}>
+          <input onChange={this.digitarTexto} type="text" name="name" />
+          <input type="submit" value="Enviar" name="name" />
+        </form>
+        <p/>
+        <Link href='/'>
+          <a>Voltar</a>
+        </Link>
+      </>
+    )
+  }
+
 }
+
+export default Page;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,60 @@
-import Link from 'next/link';
+import React from 'react'
+import Link from 'next/link'
 
-export default function Index() {
-  return (
-    <div>
-      <Link href="/about">
-        <a>About Page</a>
-      </Link>
-      <p>Hello Next.JS</p>
-    </div>
-  );
+class Index extends React.Component {
+  static getInitialProps({ query, req }) {
+    if (query.raiseError) {
+      throw new Error('Error in getInitialProps')
+    }
+  }
+
+  state = {
+    raiseError: false,
+  }
+
+  componentDidUpdate() {
+    if (this.state.raiseErrorInUpdate) {
+      throw new Error('Error in componentDidUpdate')
+    }
+  }
+
+  raiseErrorInUpdate = () => this.setState({ raiseErrorInUpdate: '1' })
+  raiseErrorInRender = () => this.setState({ raiseErrorInRender: '1' })
+
+  render() {
+    if (this.state.raiseErrorInRender) {
+      throw new Error('Error in render')
+    }
+
+    return (
+      <div>
+        <h2>Index page</h2>
+        <h4>Variavel de ambiente: {process.env.TEST_VAR}</h4>
+        <ul>
+          <li>
+            <a href="#" onClick={this.raiseErrorInRender}>
+              Raise the error in render
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={this.raiseErrorInUpdate}>
+              Raise the error in componentDidUpdate
+            </a>
+          </li>
+          <li>
+            <Link href={{ pathname: '/', query: { raiseError: '1' } }}>
+              <a>Raise error in getInitialProps of client-loaded page</a>
+            </Link>
+          </li>
+          <li>
+            <a href="/?raiseError=1">
+              Raise error in getInitialProps of server-loaded page
+            </a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
 }
+
+export default Index
